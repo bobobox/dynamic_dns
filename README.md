7e8d174cd8edfacbc5c56f8169896fc34e2d2e6c
Robin Miller <robin@bobobox.net>
2020-03-31 11:13:51 -0500

    First commit of script.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dynamic_dns
+
+Just a script for cron to run to check if my public IP has changed and update my dynamic DNS provider if necessary. Written since my new router doesn't support afraid.org for DDNS.
---
+++ dynamic-dns-monitor.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Checks for public IP(v4) changes and curls dynamic DNS udpate URL if necessary.
+# Provide update URL as first argument.
+
+set -e
+
+UPDATE_URL="$1"
+LOG_FILE='/var/log/dynamic-dns-monitor.log'
+TEMP_FILE='/tmp/.dynamic-dns-last-ip'
+
+current_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+[[ -e $TEMP_FILE ]] && last_ip=$(cat "$TEMP_FILE")
+
+if [[ $last_ip == $current_ip ]]; then
+    echo "No change"
+    exit 0
+else
+    echo "Updating IP"
+    curl "$UPDATE_URL"
+    echo "$current_ip" > "$TEMP_FILE"
+    if [[ -z $last_ip ]]; then
+        echo "$(date) - IPv4 address changed. Old IP: ${last_ip} New IP: ${current_ip}" >> "$LOG_FILE"
+    else
+        echo "$(date) - No old IPv4 address found. New IP: ${current_ip}" >> "$LOG_FILE"
+    fi
+fi
